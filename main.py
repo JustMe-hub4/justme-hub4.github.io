82d1819de9a71c9ebc6e210440850578182929a8
JustMe-hub4 <kiptooglen1@gmail.com>
2026-08-12 23:32:35 +0000

    Hide FastAPI docs and implement low credit warning UI

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ if os.getenv("SENTRY_DSN"):
     )
 
 
-app = FastAPI(title="FHIR Interop Engine", version="4.0.0")
+app = FastAPI(title="FHIR Interop Engine", version="4.0.0", docs_url=None, redoc_url=None)
 
 app.add_middleware(
     CORSMiddleware,
